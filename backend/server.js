33adee12367544ffcb641a54002135e7fff1148c
Lokendra Dangi <lokendra.dangi@himalaya-airlines.com>
2023-11-20 16:17:07 +0545

    project setup added

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+// Middleware
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  5000,
+  console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
