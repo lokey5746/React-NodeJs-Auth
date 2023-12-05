d2ea83b7171c2d370604e21ccfab22670a1ecd8e
Lokendra Dangi <lokendra.dangi@himalaya-airlines.com>
2023-12-05 16:05:32 +0545

    Frontend - mobile responsive fix

--- frontend/src/screens/auth/SignIn.jsx
+++ frontend/src/screens/auth/SignIn.jsx
@@ -3,56 +3,58 @@ import { Link } from "react-router-dom";
 
 const SignIn = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className=" max-w-lg space-y-10">
+    <div className="flex flex-col items-center justify-center h-screen p-5">
+      <div className=" max-w-lg space-y-7 md:space-y-10">
         <div className="text-white flex flex-col items-center">
           <h2 className="text-3xl font-bold">Welcome</h2>
-          <p className="text-base text-center">
+          <p className="text-xs md:text-base text-center">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id
             nesciunt mollitia nisi hic eligendi. Dignissimos facilis repellat
             nihil sequi laudantium.
           </p>
         </div>
         <div className="flex items-center space-x-5 w-ful">
-          <button className="text-red  p-4 font-semibold rounded-md shadow-md bg-btn w-full">
+          <button className="text-red text-sm md:text-lg p-3 md:p-4 font-semibold rounded-md shadow-md bg-btn w-full">
             Personal
           </button>
-          <button className="text-white  font-semibold p-4 rounded-md shadow-md bg-btn w-full">
+          <button className="text-white  font-semibold text-sm md:text-lg p-3 md:p-4 rounded-md shadow-md bg-btn w-full">
             Admin
           </button>
         </div>
         <form action="" className="space-y-5">
           <div className="flex flex-col space-y-2">
-            <label htmlFor="" className="text-gray">
+            <label htmlFor="" className="text-gray text-sm md:text-lg">
               Email Address
             </label>
             <input
               type="email"
-              className="p-4 rounded-md border border-gray text-sm bg-bg shadow-sm outline-none"
+              className="p-3 md:p-4 rounded-md border border-gray text-xs md:text-sm bg-bg shadow-sm outline-none"
               placeholder="Enter your email address"
             />
           </div>
           <div className="flex flex-col space-y-2">
-            <label htmlFor="" className="text-gray">
+            <label htmlFor="" className="text-gray text-sm md:text-lg">
               Password
             </label>
             <input
               type="password"
-              className="p-4 rounded-md border border-gray text-sm bg-bg shadow-sm outline-none"
+              className="p-3 md:p-4 rounded-md border border-gray text-xs md:text-sm bg-bg shadow-sm outline-none"
               placeholder="Enter your email password "
             />
           </div>
-          <button className="p-4 rounded-md border-gray text-sm font-semibold bg-red shadow-sm w-full text-white">
+          <button className="p-3 md:p-4 rounded-md border-gray text-xs md:text-sm font-semibold bg-red shadow-sm w-full text-white">
             Sign In
           </button>
         </form>
       </div>
       <div className="bg-btn p-6 w-full bottom-0 absolute flex justify-center">
         <div className="flex items-center space-x-5">
-          <h3 className="text-gray text-sm">Don't have an account yet?</h3>
+          <h3 className="text-gray text-xs md:text-sm">
+            Don't have an account yet?
+          </h3>
           <Link
             to="/signup"
-            className="bg-red rounded-md p-2 text-sm px-4 text-white"
+            className="bg-red rounded-md p-1 md:p-2 text-xs md:text-sm px-4 text-white"
           >
             Sign Up
           </Link>
